2b43f636b4cc805efefd4e3716aa306720b44994
CodeXNilanshu <86039482+CodeXNilanshu@users.noreply.github.com>
2026-06-20 23:49:45 +0530

    Learning Basics once again

---
+++ input.py
@@ -0,0 +1,15 @@
+input("what is your name?")
+
+##The above input takes our input and then stop the program, it doesn't display the output so we will use print statement for displaying the output
+
+## Input ask for the place we are from and then input store it within itself  and then we used print to display the stored thing inside of input on the screen
+print(input("Where are you from?"))
+
+
+print('Hello', input('Enter your name'))
+
+
+
+
+'''This can also be used to write comment but normal comments are better for production cuz Unassigned String Literals: You enclose text inside triple quotes. If a string literal is not assigned to a variable, Python parses it but ignores it during execution'''
+
---
+++ print.py
@@ -0,0 +1,23 @@
+#I am using ## so that I can remember I wrote this for my notes
+
+print("Hello Nilanshu")
+
+## We can also use semicolon in the last and it won't show any error but it gives us warning - basically warning tells us that this is not the usually necessary or we don't follow this syntax
+print('Hello Semicolon');
+
+## Python adds space automatically, when we use comma and it is called seperator
+print('Hello','Nilanshu')
+
+## If we don't want default space then we can write sep='' and space will disappear
+print("Hello", "Nilanshu", sep='')
+
+## We can write down anything in the seperator and we can use double quote "" and single quote '' together like example below
+print("Hello", "Nilanshu", 'Super Cool' , sep=' : : ')
+
+## Now all the outputs are in the next line but if we want them in single line then we use end= it tells that we want the next output in the same line as this output, by default it goes to the next line but we don't want that.
+
+## These end and seperators are specific to one print statement
+print("Hello", "Nilanshu", end='')
+print('Hello', 'Nilanshu2', end = ' -> ')
+print('Bye', 'Nilanshu3')
+
